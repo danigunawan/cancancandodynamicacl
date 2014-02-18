8041dcc133307cef4ca6373304d7413536fca11b
David Frey <david@cloudability.com>
2014-02-18 10:22:48 -0800

    Authorize report viewing.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,16 @@
 class ReportsController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :authorized_to_view_report?, only: [:show]
   def index
     @reports = Report.all
   end
 
   def show
     @report = Report.find(params[:id])
+  end
+
+  private
+  def authorized_to_view_report?
+    authorize! :read, "report_#{params[:id]}".to_sym
   end
 end
