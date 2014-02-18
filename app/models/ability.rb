e44efce68c56fb5e57d64c85b5346491d241ad2b
David Frey <david@cloudability.com>
2014-02-18 10:22:27 -0800

    Meta authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,5 +6,23 @@ class Ability
     if user.has_role? :admin
       can :read, :admin
     end
+
+    # Meta Program Report Authorizations
+    # This should probably be cached within the session
+    # and invalidated somehow when the AuthorizedCollaborator
+    # is updated in scope to this Organization.
+    accessible_report_ids(user).each do |id|
+      can :read, "report_#{id}".to_sym
+    end
+  end
+
+  private
+  def accessible_report_ids(user)
+    ids = []
+    user.roles.each do |role|
+      ids |= role.collaborations.where(authorizable_type: 'Report').map(&:authorizable_id)
+    end
+
+    ids | user.collaborations.where(authorizable_type: 'Report').map(&:authorizable_id)
   end
 end
